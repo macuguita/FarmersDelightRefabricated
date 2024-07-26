1501f4c53baa7dcb51298c9ef688f784516f5df3
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-07-26 17:34:33 +0200

    deleted compat package

--- src/main/java/com/nhoryzon/mc/farmersdelight/entity/block/HeatableBlockEntity.java
+++
@@ -1,6 +0,0 @@
-package com.nhoryzon.mc.farmersdelight.entity.block;
-
-@Deprecated(forRemoval = true)
-public interface HeatableBlockEntity extends vectorwing.farmersdelight.common.block.entity.HeatableBlockEntity {
-
-}
