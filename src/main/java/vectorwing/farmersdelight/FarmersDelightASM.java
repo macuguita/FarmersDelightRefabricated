1effb3285d353c6c7064a9e91c36ae8fbd3af63b
Anastasia Chrysanthemum <cow@chrysanthemum.lgbt>
2024-08-25 13:06:27 +1000

    Remove Knife Enchantment category.

--- src/main/java/vectorwing/farmersdelight/FarmersDelightASM.java
+++ src/main/java/vectorwing/farmersdelight/FarmersDelightASM.java
@@ -7,15 +7,12 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import vectorwing.farmersdelight.common.registry.ModItems;
 
-import java.util.List;
-
 public class FarmersDelightASM implements Runnable {
     public static final String COOKING_RECIPE_BOOK_TYPE = "FARMERSDELIGHT_COOKING";
     public static final String COOKING_SEARCH_RECIPE_BOOK_CATEGORY = "FARMERSDELIGHT_COOKING_SEARCH";
     public static final String COOKING_MEALS_RECIPE_BOOK_CATEGORY = "FARMERSDELIGHT_COOKING_MEALS";
     public static final String COOKING_DRINKS_RECIPE_BOOK_CATEGORY = "FARMERSDELIGHT_COOKING_DRINKS";
     public static final String COOKING_MISC_RECIPE_BOOK_CATEGORY = "FARMERSDELIGHT_COOKING_MISC";
-    public static final String KNIFE_ENCHANTMENT_CATEGORY = "FARMERSDELIGHT_KNIFE";
 
     @Override
     public void run() {
@@ -28,8 +25,6 @@ public class FarmersDelightASM implements Runnable {
         ClassTinkerers.enumBuilder(recipeBookCategoriesTarget, itemStackParamType).addEnum(COOKING_MEALS_RECIPE_BOOK_CATEGORY, () -> new Object[]{new ItemStack[]{new ItemStack(ModItems.VEGETABLE_NOODLES.get())}}).build();
         ClassTinkerers.enumBuilder(recipeBookCategoriesTarget, itemStackParamType).addEnum(COOKING_DRINKS_RECIPE_BOOK_CATEGORY,() -> new Object[]{new ItemStack[]{new ItemStack(ModItems.APPLE_CIDER.get())}}).build();
         ClassTinkerers.enumBuilder(recipeBookCategoriesTarget, itemStackParamType).addEnum(COOKING_MISC_RECIPE_BOOK_CATEGORY, () -> new Object[]{new ItemStack[]{new ItemStack(ModItems.DUMPLINGS.get()), new ItemStack(ModItems.TOMATO_SAUCE.get())}}).build();
-        String enchantmentTarget = remapper.mapClassName("intermediary", "net.minecraft.class_1886");
-        ClassTinkerers.enumBuilder(enchantmentTarget).addEnumSubclass(KNIFE_ENCHANTMENT_CATEGORY, "vectorwing.farmersdelight.common.item.enchantment.category.KnifeEnchantmentCategory").build();
     }
 
 }
