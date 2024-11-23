2053d6a5de850b182d7ab498d816c9d58f3d118a
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-11-23 15:20:29 +0100

    improved flipping animation fixed filpping animation interpolation (1.21) skillet wont play update item animation no more when cooking or flipping

--- src/main/java/vectorwing/farmersdelight/client/renderer/SkilletItemRenderer.java
+++ src/main/java/vectorwing/farmersdelight/client/renderer/SkilletItemRenderer.java
@@ -2,7 +2,6 @@ package vectorwing.farmersdelight.client.renderer;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
-import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRenderer;
 import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -16,43 +15,59 @@ import vectorwing.farmersdelight.common.item.component.ItemStackWrapper;
 import vectorwing.farmersdelight.common.registry.ModDataComponents;
 
 public class SkilletItemRenderer implements BuiltinItemRendererRegistry.DynamicItemRenderer {
-	public SkilletItemRenderer() {
-	}
+    public SkilletItemRenderer() {
+    }
 
-	@Override
-	public void render(ItemStack stack, ItemDisplayContext mode, PoseStack poseStack, MultiBufferSource buffer, int packedLight, int packedOverlay) {
-		//render block
-		poseStack.pushPose();
-		BlockItem item = ((BlockItem) stack.getItem());
-		BlockState state = item.getBlock().defaultBlockState();
-		Minecraft.getInstance().getBlockRenderer().renderSingleBlock(state, poseStack, buffer, packedLight, packedOverlay);
-		poseStack.popPose();
+    @Override
+    public void render(ItemStack stack, ItemDisplayContext mode, PoseStack poseStack, MultiBufferSource buffer, int packedLight, int packedOverlay) {
+        //render block
+        BlockItem item = ((BlockItem) stack.getItem());
+        BlockState state = item.getBlock().defaultBlockState();
 
-		ItemStackWrapper stackWrapper = stack.getOrDefault(ModDataComponents.SKILLET_INGREDIENT.get(), ItemStackWrapper.EMPTY);
-		ItemStack ingredientStack = stackWrapper.getStack();
 
-		if (!ingredientStack.isEmpty()) {
-			poseStack.pushPose();
-			poseStack.translate(0.5, 1 / 16f, 0.5);
+        Minecraft mc = Minecraft.getInstance();
 
-			long gameTime = Minecraft.getInstance().level.getGameTime();
-			if (stack.has(ModDataComponents.SKILLET_FLIP_TIMESTAMP.get())) {
-				long time = stack.get(ModDataComponents.SKILLET_FLIP_TIMESTAMP.get());
-				float partialTicks = Minecraft.getInstance().getTimer().getRealtimeDeltaTicks();
-				float animation = ((gameTime - time) + partialTicks) / SkilletItem.FLIP_TIME;
-				float maxH = 1;
-				poseStack.translate(0, maxH * Mth.sin(animation * Mth.PI), 0);
-				poseStack.mulPose(Axis.XP.rotationDegrees(360 * animation));
-			}
+        ItemStackWrapper stackWrapper = stack.getOrDefault(ModDataComponents.SKILLET_INGREDIENT.get(), ItemStackWrapper.EMPTY);
+        ItemStack ingredientStack = stackWrapper.getStack();
 
-			poseStack.mulPose(Axis.XP.rotationDegrees(90));
-			poseStack.scale(0.5F, 0.5F, 0.5F);
+        float animation = 0;
 
-			var itemRenderer = Minecraft.getInstance().getItemRenderer();
-			itemRenderer.renderStatic(ingredientStack, ItemDisplayContext.FIXED, packedLight,
-					packedOverlay, poseStack, buffer, null, 0);
+        if (!ingredientStack.isEmpty()) {
+            poseStack.pushPose();
+            poseStack.translate(0.5, 1 / 16f, 0.5);
 
-			poseStack.popPose();
-		}
-	}
+            long gameTime = mc.level.getGameTime();
+            if (stack.has(ModDataComponents.SKILLET_FLIP_TIMESTAMP.get())) {
+                long time = stack.get(ModDataComponents.SKILLET_FLIP_TIMESTAMP.get());
+                float partialTicks = mc.getTimer().getGameTimeDeltaPartialTick(false);
+                animation = ((gameTime - time) + partialTicks) / SkilletItem.FLIP_TIME;
+                animation = Mth.clamp(animation, 0, 1);
+                float maxH = 1;
+                poseStack.translate(0, maxH * Mth.sin(animation * Mth.PI), 0);
+                poseStack.mulPose(Axis.XP.rotationDegrees(360 * animation));
+            }
+
+            poseStack.mulPose(Axis.XP.rotationDegrees(90));
+            poseStack.scale(0.5F, 0.5F, 0.5F);
+
+            var itemRenderer = mc.getItemRenderer();
+            itemRenderer.renderStatic(ingredientStack, ItemDisplayContext.FIXED, packedLight,
+                    packedOverlay, poseStack, buffer, null, 0);
+
+            poseStack.popPose();
+        }
+
+        poseStack.pushPose();
+
+        if (animation != 0) {
+            poseStack.translate(0, 0, 1);
+            poseStack.mulPose(Axis.XP.rotationDegrees(Mth.sin(animation * Mth.TWO_PI) * 20));
+            poseStack.translate(0F, 0, -1);
+            poseStack.translate(0, 0, -Mth.sin(animation * Mth.PI) * 0.3);
+        }
+        mc.getBlockRenderer().renderSingleBlock(state, poseStack, buffer, packedLight, packedOverlay);
+
+        poseStack.popPose();
+
+    }
 }
